f09b232ea6aae413add91ca7c53b3493b83ea3c5
focuslegon <33376436+focuslegon@users.noreply.github.com>
2024-03-03 08:40:01 +0000

    fixes

--- resources/views/amicrud/table/export.blade.php
+++ resources/views/amicrud/table/export.blade.php
@@ -1,45 +1,51 @@
 
-
-    <table class="table align-middle table-nowrap" id="amicrudTable" >
-        <!-- Table Head -->
-        <thead class="table-light">
-            <tr>
-                <th scope="col" style="width: 50px;">#</th>
-                @forelse($display_field as $column_name => $custom_name)
-                <th class="sort" data-sort="{{$custom_name}}">{{ amicrud_form_labels($custom_name) }}</th>
-                @empty
-                @endforelse
+<div>
+    @include('amicrud::amicrud.table.export-head')
+    <div class="table-container">
+    <div class="table-responsive table-card mt-3 mb-1">
+        <table class="table align-middle table-nowrap" id="amicrudTable" >
+            <!-- Table Head -->
+            <thead class="table-light">
+                <tr>
+                    <th scope="col" style="width: 50px;">#</th>
+                    @forelse($display_field as $column_name => $custom_name)
+                    <th class="sort" data-sort="{{$custom_name}}">{{ amicrud_form_labels($custom_name) }}</th>
+                    @empty
+                    @endforelse
+                    
+                </tr>
+            </thead>
+                                <!-- Table Body -->
+            <tbody class="list form-check-all">
+                @forelse($list_contents as $k => $c)
+                <tr data-id="{{ $c->id }}">
+                    <td scope="row">
+                        {{$k+1}}
+                    </td>
+                    @forelse($display_field as $column_name => $custom_name)
+                    <td>
+                    @if ($formable[$column_name]['type']=='file')
+                    <div class="flex-shrink-0">
+                        <a href="{{asset($c->{$column_name})}}" target="_blank">
+                        Open File
+                        </a>
+                    </div>
+                    @elseif(in_array($column_name,['status','flow_status','action_status']))
+                    <span class="btn badge bg-soft-{{ amicrud_status_class($c->{$column_name}) }} text-{{ amicrud_status_class($c->{$column_name}) }}">{{ $c->{$column_name} }}</span>
+                    @elseif($formable[$column_name]['type']=='select')
+                        {{ $c->{$column_name} ? amicrud_form_labels(ucfirst($formable[$column_name]['select_items'][$c->{$column_name}])) :null }}
+                    @else 
+                    {!! amicrud_form_labels($c->{$column_name}) !!}
+                    @endif
+                    </td>
+                    @empty
+                    @endforelse
                 
-            </tr>
-        </thead>
-                            <!-- Table Body -->
-         <tbody class="list form-check-all">
-            @forelse($list_contents as $k => $c)
-            <tr data-id="{{ $c->id }}">
-                <td scope="row">
-                    {{$k+1}}
-                </td>
-                @forelse($display_field as $column_name => $custom_name)
-                 <td>
-                   @if ($formable[$column_name]['type']=='file')
-                   <div class="flex-shrink-0">
-                    <a href="{{asset($c->{$column_name})}}" target="_blank">
-                    Open File
-                    </a>
-                  </div>
-                   @elseif(in_array($column_name,['status','flow_status','action_status']))
-                   <span class="btn badge bg-soft-{{ amicrud_status_class($c->{$column_name}) }} text-{{ amicrud_status_class($c->{$column_name}) }}">{{ $c->{$column_name} }}</span>
-                   @elseif($formable[$column_name]['type']=='select')
-                    {{ $c->{$column_name} ? amicrud_form_labels(ucfirst($formable[$column_name]['select_items'][$c->{$column_name}])) :null }}
-                   @else 
-                   {!! amicrud_form_labels($c->{$column_name}) !!}
-                   @endif
-                 </td>
+                </tr>
                 @empty
                 @endforelse
-            
-            </tr>
-            @empty
-            @endforelse
-        </tbody>
-   </table>
+            </tbody>
+    </table>
+    </div>
+    </div>
+</div>
